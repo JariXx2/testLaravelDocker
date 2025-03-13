b15b036e7a4a07065e2df44c5ff4dc328f8da9c3
JariXx2 <korelskiy_2000@mail.ru>
2025-03-13 16:18:32 +0300

    fix python

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -8,6 +8,14 @@ client = MongoClient('mongodb://root:root@mongodb:27017/')
 db = client['mydatabase']
 collection = db['mycollection']
 
+# Функция для добавления данных в MongoDB
+@app.route('/api/add', methods=['POST'])
+def add_data():
+    data = request.json
+    result = collection.insert_one(data)
+    return jsonify({'message': f'Данные добавлены с ID: {result.inserted_id}'})
+
+# Функция для получения данных из MongoDB
 @app.route('/api/data', methods=['GET'])
 def get_data():
     data = collection.find_one()
